b4711e2fd356b24d2e82d3848f4a3d8094bc39b8
LisandrOviedo <lisandroviedo@hotmail.com>
2025-11-11 16:21:32 -0400

    Add: Servidor con ruta "/" para obtener las últimas tasas vigentes de las principales monedas reflejadas en el BCV.

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const morgan = require("morgan");
+const { fecha_hora_actual } = require("./dayjs");
+
+const https = require("https");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const NodeCache = require("node-cache");
+const cache = new NodeCache({ stdTTL: 900 }); // Cache para almacenar las tasas por 15 minutos
+
+const URL_BCV = "https://www.bcv.org.ve/";
+
+const app = express();
+
+app.disable("x-powered-by");
+
+const port = 4055;
+
+morgan.token("fecha_hora_actual", () => fecha_hora_actual());
+
+app.use(morgan(":fecha_hora_actual :method :url :status :response-time ms"));
+
+app.use(express.json({ limit: "65mb" })); //Límite máximo en el tamaño de los datos JSON que el servidor puede manejar de una sola vez, para evitar posibles ataques de denegación de servicio (DoS) o abusos.
+
+const formatNumber = (number) => Number(number.replace(",", "."));
+
+const getBCVRates = async (req, res) => {
+  const cached = cache.get("tasas");
+
+  if (cached) return res.json(cached);
+
+  try {
+    const response = await axios.get(URL_BCV, {
+      httpsAgent: new https.Agent({
+        rejectUnauthorized: false,
+      }),
+    });
+
+    if (response && response.data) {
+      const $ = cheerio.load(response.data);
+
+      const eur_currency = $("#euro strong").text();
+      const cny_currency = $("#yuan strong").text();
+      const try_currency = $("#lira strong").text();
+      const rub_currency = $("#rublo strong").text();
+      const usd_currency = $("#dolar strong").text();
+
+      const tasas = {
+        current_time_vzla: fecha_hora_actual(),
+        eur: formatNumber(eur_currency),
+        cny: formatNumber(cny_currency),
+        try: formatNumber(try_currency),
+        rub: formatNumber(rub_currency),
+        usd: formatNumber(usd_currency),
+      };
+
+      cache.set("tasas", tasas);
+
+      res.json(tasas);
+    }
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({ error: error.message });
+  }
+};
+
+app.get("/", getBCVRates);
+
+app.listen(port, () => {
+  console.log(`App listening on http://localhost:${port}`);
+});
